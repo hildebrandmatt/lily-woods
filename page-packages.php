965cdc7e01d89cc301ef51d2308cc51c7df82b93
Matt Hildebrand <hildebrand.matt@gmail.com>
2021-02-23 21:07:34 -0800

    Packages page content added

--- page-packages.php
+++ page-packages.php
@@ -18,14 +18,53 @@ get_header();
 	<main id="primary" class="site-main">
 
 		<?php
-		while ( have_posts() ) :
-			the_post();
+            $args = array(
+                'post_type' => 'lily-packages',
+                'posts_per_page' => -1,
+            );
 
-			get_template_part( 'template-parts/content', 'page' );
+            $query = new WP_Query( $args );
 
+            if ( $query->have_posts() ) {
+                while ( $query->have_posts() ) {
+                    $query->the_post();
 
-		endwhile; // End of the loop.
-		?>
+                    ?><section class="package-section">
+						<h3> <?php the_title() ?> </h3> <?php
+						if ( get_field('package_price') ){
+							?><p> <?php the_field('package_price') ?> </p><?php
+						}
+						if ( get_field('package_duration') ){
+							?><p> <?php the_field('package_duration') ?> </p><?php
+						}
+						if ( get_field('package_description') ){
+							?><p> <?php the_field('package_description') ?> </p><?php
+						}
+					?></section><?php
+
+                }
+                wp_reset_postdata();
+            }
+
+			?><section id="faq-section">
+			<h2>FAQ</h2>
+				<?php
+				// Check rows exists.
+				if( have_rows('faq_repeater') ):
+					// Loop through rows.
+					while( have_rows('faq_repeater') ) : the_row();
+						// Load sub field value.
+						?><article class="faq-question">
+							<h4><?php the_sub_field('question'); ?></h4>
+							<p><?php the_sub_field('answer'); ?></p>
+						</article>
+					<?php endwhile;
+				// No value.
+				else :
+					// Do something...
+				endif; ?>
+			</section> 
+        ?>
 
 	</main><!-- #main -->
 
