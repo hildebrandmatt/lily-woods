235f21643f7e07d2d02c4d2188f25f7530589ba2
Matt Hildebrand <hildebrand.matt@gmail.com>
2021-03-05 12:57:20 -0800

    Mobile hamburger icon

--- header.php
+++ header.php
@@ -46,7 +46,11 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'lily-woods' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+				<span></span>
+				<span></span>
+				<span></span>
+			</button>
 			<?php
 			wp_nav_menu(
 				array(
