8140c1f4d7070cbe2812cf7ebcf1bf05c67fb9df
nyanbeans <66818117+nyanbeans@users.noreply.github.com>
2021-03-05 16:11:30 -0800

    start of the final tasks :^)

--- 404.php
+++ 404.php
@@ -18,38 +18,8 @@ get_header();
 			</header><!-- .page-header -->
 
 			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'lily-woods' ); ?></p>
+				<p class="oops-text"><?php esc_html_e( 'It looks like nothing was found at this location.', 'lily-woods' ); ?></p>
 
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'lily-woods' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$lily_woods_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'lily-woods' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$lily_woods_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
 
 			</div><!-- .page-content -->
 		</section><!-- .error-404 -->
@@ -57,4 +27,3 @@ get_header();
 	</main><!-- #main -->
 
 <?php
-get_footer();
