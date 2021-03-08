83f7e2f13ba0ad7bdb7d23ed1e6650c2666f9b24
Matt Hildebrand <hildebrand.matt@gmail.com>
2021-03-08 10:51:46 -0800

    Added google analytics code

--- header.php
+++ header.php
@@ -18,6 +18,15 @@
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
 	<?php wp_head(); ?>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-2PVCHWX085"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'G-2PVCHWX085');
+	</script>
 </head>
 
 <body <?php body_class(); ?>>
